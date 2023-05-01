66eae5c5d511eef00cb58524ccdeb7295a125df8
iasadcms <84484117+iasadcms@users.noreply.github.com>
2023-05-01 03:08:14 +0100

    Update pstetris.ps1

--- pstetris.ps1
+++ pstetris.ps1
@@ -1738,49 +1738,32 @@ for ($idx = 0; $idx -lt $CONBUFF_SIZE; $idx++) {
 # BEGIN
 #######################################################################################################
 
-function AdjustConsoleSize() {
-    Param(
-        [Parameter(Mandatory=$False,Position=0)]
-        [int]$Height = 40,
-        [Parameter(Mandatory=$False,Position=1)]
-        [int]$Width = 120
-        )
-   $console = $host.ui.rawui
-   $ConBuffer  = $console.BufferSize
-   $ConSize = $console.WindowSize   
-   $currWidth = $ConSize.Width
-   $currHeight = $ConSize.Height   
-   # if height is too large, set to max allowed size
-   if ($Height -gt $host.UI.RawUI.MaxPhysicalWindowSize.Height) {
-       $Height = $host.UI.RawUI.MaxPhysicalWindowSize.Height
-   }   
-   # if width is too large, set to max allowed size
-   if ($Width -gt $host.UI.RawUI.MaxPhysicalWindowSize.Width) {
-       $Width = $host.UI.RawUI.MaxPhysicalWindowSize.Width
-   }   
-   # If the Buffer is wider than the new console setting, first reduce the width
-   If ($ConBuffer.Width -gt $Width ) {
-      $currWidth = $Width
-   }
-   # If the Buffer is higher than the new console setting, first reduce the height
-   If ($ConBuffer.Height -gt $Height ) {
-       $currHeight = $Height
-   }
-   # initial resizing if needed
-   $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.size($currWidth,$currHeight)   
-   # Set the Buffer
-   $host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.size($Width,2000)   
-   # Now set the WindowSize
-   $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.size($Width,$Height)   
+
+function AdjustConsoleSize([int]$Height = 40, [int]$Width = 120) {
+    $console = $host.UI.RawUI
+    $ConBuffer  = $console.BufferSize
+    $ConSize = $console.WindowSize
+    $currWidth = $ConSize.Width
+    $currHeight = $ConSize.Height
+    $Height = [System.Math]::Min($Height, $host.UI.RawUI.MaxPhysicalWindowSize.Height)
+    $Width = [System.Math]::Min($Width, $host.UI.RawUI.MaxPhysicalWindowSize.Width)
+    If ($ConBuffer.Width -gt $Width ) {
+       $currWidth = $Width
+    }
+    If ($ConBuffer.Height -gt $Height ) {
+        $currHeight = $Height
+    }
+    $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.size($currWidth,$currHeight)
+    $host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.size($Width, 2000)
+    $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.size($Width,$Height)    
 }
 
 
 try {
     ## init screen
     $SAVED_CONSOLESIZE = $Host.UI.RawUI.WindowSize
-    AdjustConsoleSize -Height ($CONBUFF_HEIGHT + 1) -Width ($CONBUFF_WIDTH + 1)
-    
     SetCursorVisible -Visible $false
+    AdjustConsoleSize -Height ($CONBUFF_HEIGHT + 1) -Width ($CONBUFF_WIDTH + 1)    
     Clear-Host
     [console]::SetCursorPosition(0, 0)    
     ResetLastConBuff        # init last print buffer to a char that's unliklely to be printed
